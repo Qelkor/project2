b7eecbbe6a211e3d4470bd6195403de6a93bc63b
Qelkor <103414801+Qelkor@users.noreply.github.com>
2022-07-20 10:44:21 +0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,38 +2,42 @@ import React, { Link } from "react-router-dom";
 import { useRef, useState } from "react";
 import Totoro from "../media/totoro.mp3";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCirclePlay, faCirclePause } from "@fortawesome/free-regular-svg-icons";
-
+import {
+  faCirclePlay,
+  faCirclePause,
+} from "@fortawesome/free-regular-svg-icons";
 
 function NavBar() {
-
   const [playing, setPlaying] = useState(false);
   const audioPlayer = useRef();
 
-const toggle = () => {
-  const prevValue = playing;
-  setPlaying(!prevValue);
-  if (!prevValue) {
-    audioPlayer.current.play();
-  } else {
-    audioPlayer.current.pause();
-  }
-}
+  const toggle = () => {
+    const prevValue = playing;
+    setPlaying(!prevValue);
+    if (!prevValue) {
+      audioPlayer.current.play();
+    } else {
+      audioPlayer.current.pause();
+    }
+  };
 
   return (
-      <nav>
-        <Link to="/Home">
-          <img src="https://i.imgur.com/woHZhCw.png" alt="" />
-        </Link>
-        <Link to="/Films">Films</Link>
-        <Link to="/Locations">Locations</Link>
-        <Link to="/People">People</Link>
-        <audio ref={audioPlayer} src={Totoro} />
-        <button className="Icon" onClick={toggle}>
-          {playing ? <FontAwesomeIcon icon={faCirclePlay} /> :
-          <FontAwesomeIcon icon={faCirclePause} />}
-        </button>
-      </nav>
+    <nav>
+      <Link to="/Home">
+        <img src="https://i.imgur.com/oOXszW5.png" alt="" />
+      </Link>
+      <Link to="/Films">Films</Link>
+      <Link to="/Locations">Locations</Link>
+      <Link to="/People">People</Link>
+      <audio ref={audioPlayer} src={Totoro} />
+      <button className="Icon" onClick={toggle}>
+        {playing ? (
+          <FontAwesomeIcon icon={faCirclePlay} />
+        ) : (
+          <FontAwesomeIcon icon={faCirclePause} />
+        )}
+      </button>
+    </nav>
   );
 }
 
